a1642f35e8db805a87d8a923556aa257dbd5b21e
aCortinaU <acortinau@gmail.com>
2022-09-28 22:59:43 +0200

    start working in black border

--- utils/plots.py
+++ utils/plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-def get_image(img_num: str, path: str):
+def get_image_and_plot(img_num: str, path: str):
     """ Load an image and plots it
 
     Args:
@@ -17,3 +17,8 @@ def get_image(img_num: str, path: str):
     plt.imshow(image)
     plt.axis("off")
     return image
+
+def get_image(img_num: str, path: str):
+    path = path + "/nev" + img_num + ".jpg"
+    return cv2.imread(path, cv2.IMREAD_COLOR)
+    
